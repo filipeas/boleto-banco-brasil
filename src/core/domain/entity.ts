01ed8941e43366b1e9b4d9b418831c1375045467
filipeas <felipealvessampaio@hotmail.com>
2022-11-19 09:21:32 -0300

    fix: remove src dir

--- src/core/domain/entity.ts
+++
@@ -1,7 +0,0 @@
-export abstract class Entity<T>{
-    protected props: T;
-
-    constructor(props: T){
-        this.props = props;
-    }
-}
--- src/core/logic/maybe.ts
+++
@@ -1,2 +0,0 @@
-export type Maybe<T> = T | null | undefined; 
-export type AsyncMaybe<T> = Promise<Maybe<T>>;
